aef8775896c3dfcec528d287ad73bb74a50a0a1a
Nikita Melnik <nekit.melnik@gmail.com>
2023-04-10 11:44:49 +0300

    Add Thread Basics example

---
+++ src/clr-via-csharp/src/CLRViaCSharp.Threading/Examples/ThreadBasics.cs
@@ -0,0 +1,60 @@
+﻿using CLRViaCSharp.Common.Interfaces;
+
+namespace CLRViaCSharp.Threading.Examples;
+
+internal class ThreadBasics : IExample
+{
+    /// <inheritdoc />
+    public static void Run()
+    {
+        // DedicatedThread.CreateADedicatedThread();
+        BackgroundVsForeground.Run();
+    }
+}
+
+internal static class DedicatedThread
+{
+    public static void CreateADedicatedThread()
+    {
+        Console.WriteLine("Main thread: starting a dedicated thread to do an asynchronous operation");
+        var dedicatedThread = new Thread(ComputeBoundOp);
+        dedicatedThread.Start(5);
+        Console.WriteLine("Main thread: doing other work here...");
+        Thread.Sleep(10000); // Simulating other work (10 seconds)
+        dedicatedThread.Join(); // Wait for thread to terminate
+        Console.WriteLine("Hit <Enter> to end this program");
+        Console.ReadLine();
+    }
+
+    // This method's signature must match the PaarameterizedThreadStart delegate
+    private static void ComputeBoundOp(object? state)
+    {
+        // This method is executed by a dedicated thread
+        Console.WriteLine("In ComputeBoundOp: state={0}", state);
+        Thread.Sleep(1000); // Simulates other work (1 second)
+        // When this method returns, the dedicated thread dies
+    }
+}
+
+internal static class BackgroundVsForeground
+{
+    public static void Run()
+    {
+        // Create a new thread (defaults to foreground)
+        var t = new Thread(Worker);
+        // Make the thread a background thread
+        t.IsBackground = true;
+
+        // Start the thread
+        t.Start();
+        // If t is a foreground thread, the application won't die fro about 10 seconds
+        // If t s a background thread, the application dies immediately
+        Console.WriteLine("returning from Run method");
+    }
+
+    private static void Worker()
+    {
+        Thread.Sleep(10000);
+        Console.WriteLine("Returning from Worker");
+    }
+}
---
+++ src/clr-via-csharp/src/CLRViaCSharp.Threading/README.md
@@ -0,0 +1,52 @@
+﻿# Threading
+
+1. [Thread Basics](#thread-basics)
+2. [Compute-Bound Asynchronous Operations](#compute-bound-asynchronous-operations)
+3. [I/O-Bound Asynchronous Operations](#hybrid-thread-synchronization-constructs)
+4. [Primitive Thread Synchronization Constructs](#io-bound-asynchronous-operations)
+5. [Hybrid Thread Synchronization Constructs](#primitive-thread-synchronization-constructs)
+
+## Thread basics
+
+A thread is a Windows concept whose job is to virtualize the CPU.
+
+#### Thread Overhead
+
+Every thread has one of each of the following:
+
+- Thread kernel object
+- Thread environment block (TEB)
+- User-mode stack
+- Kernel-mode stack
+- DLL thread-attach and thread-detach notifications
+
+At any given moment in time, Windows assigns one thread to a CPU. When the time-slice of the thread expires, windows
+context switches to another thread. Every context-switch requires that Windows performs the following actions
+
+1) Save the values in the CPU's registers to the currently running thread's context structure inside the thread's
+   kernel object.
+2) Select one thread from the set of existing threads to schedule next.
+3) Load the values in the selected thread's context structure into the CPU's registers.
+
+#### Reasons to use threads
+
+- Responsiveness (typically for client-side GUI applications)
+- Performance (for client and serer side applications)
+
+[Back to top ⇧](#threading)
+
+## Compute-Bound Asynchronous Operations
+
+[Back to top ⇧](#threading)
+
+## I/O-Bound Asynchronous Operations
+
+[Back to top ⇧](#threading)
+
+## Primitive Thread Synchronization Constructs
+
+[Back to top ⇧](#threading)
+
+## Hybrid Thread Synchronization Constructs
+
+[Back to top ⇧](#threading)
